60cd1bde7f3d565791b02e354189587a5190be3b
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-07-26 15:49:14 +0530

    first commit

---
+++ Bank.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16
+{
+
+    public class BankAccount
+    {
+        private readonly int AccountNumber;
+        public int accountnumber
+        {
+            get { return AccountNumber; }
+        }
+        private string AccountHolderName;
+        public string accountholdername
+        {
+            get { return AccountHolderName; }
+            set { AccountHolderName = value; }
+        }
+        private double Balance;
+
+
+
+        public BankAccount(string name, int num)
+        {
+            this.AccountHolderName = name;
+            this.AccountNumber = num;
+            this.Balance = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposited {amount}.\nNew balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount. Amount must be greater than 0.");
+            }
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdrawn {amount}.\nNew balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount or insufficient funds.");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Deva", 764);
+           
+
+            Console.WriteLine($"details of account 1 :\nAccount holder name :{account.accountholdername}\nAccount number : {account.accountnumber}");
+            Console.WriteLine("Enter the Amount you want to deposit ");
+            account.Deposit(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Enter how much amount you want to with draw");
+            account.Withdraw(double.Parse(Console.ReadLine())); 
+            Console.ReadKey();
+        }
+    }
+}
